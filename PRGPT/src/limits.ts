f596cf5580b531c561a3f78391995da61c96d7b3
Subhajit Pathak <subhajitpathak9900@gmail.com>
2026-04-11 00:08:43 +0530

    Fix PRGPT execution files indexing

---
+++ PRGPT/src/limits.ts
@@ -0,0 +1,35 @@
+export class TokenLimits {
+  maxTokens: number
+  requestTokens: number
+  responseTokens: number
+  knowledgeCutOff: string
+
+  constructor(model = 'gpt-4o-mini') {
+    this.knowledgeCutOff = '2023-11-01'
+    if (model.includes('gpt-4o') && !model.includes('mini')) {
+      this.maxTokens = 128000
+      this.responseTokens = 16000
+    } else if (model.includes('gpt-4o-mini')) {
+      this.maxTokens = 128000
+      this.responseTokens = 16000
+    } else if (model === 'gpt-4-32k') {
+      this.maxTokens = 32600
+      this.responseTokens = 4000
+    } else if (model === 'gpt-3.5-turbo-16k') {
+      this.maxTokens = 16300
+      this.responseTokens = 3000
+    } else if (model === 'gpt-4') {
+      this.maxTokens = 8000
+      this.responseTokens = 2000
+    } else {
+      this.maxTokens = 4000
+      this.responseTokens = 1000
+    }
+    // provide some margin for the request tokens
+    this.requestTokens = this.maxTokens - this.responseTokens - 100
+  }
+
+  string(): string {
+    return `max_tokens=${this.maxTokens}, request_tokens=${this.requestTokens}, response_tokens=${this.responseTokens}`
+  }
+}
